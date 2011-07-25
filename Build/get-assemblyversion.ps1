4d73885c1b8dfdf677ca791587d3a1d340f7fd27
Johan Andersson <johan-spam@andersson.net>
2011-07-25 07:29:21 +0200

    Added scripts for parsing and manipulating the AssemblyVersionAttribute defined in a .cs file

---
+++ Build/get-assemblyversion.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+	Reads the AssemblyVersion attribute from a .CS file and returns an object representation of the version
+.DESCRIPTION
+	Specify the path to the .CS file and returned is an object with the properties Version (complete version number in a dotted format), Major, Minor, Build and Revision numbers.
+#>
+param(
+	[parameter(mandatory=$true)]
+	[string] $assemblyInfoFile 
+)
+
+$version = "" | select Major, Minor, Build, Revision | Add-Member -PassThru -Name Version -MemberType ScriptProperty -Value { "$($this.Major).$($this.Minor).$($this.Build).$($this.Revision)" }
+
+gc $assemblyInfoFile | %{ 
+	if( $_ -match "AssemblyVersion(Attribute)?\s*\(\s*`"(\d+)\.(\d+)\.(\d+)\.(\d+)`"\s*\)" ) {
+		$version.Major = [int]$matches[2]
+		$version.Minor = [int]$matches[3]
+		$version.Build = [int]$matches[4]
+		$version.Revision = [int]$matches[5]
+	}
+}
+
+$version
---
+++ Build/set-assemblyversion.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+	Writes the AssemblyVersion attribute version to a specified .CS file path
+.DESCRIPTION
+	Given an existing .CS file containing an AssemblyVersion attribute, this script will update its version number according to the parameters specified.
+#>
+param(
+	[parameter(position=0, mandatory=$true)]
+	[string] $assemblyInfoFile,
+	[parameter(mandatory=$true, parametersetname="GivenNumber", helpmessage="The major part of the version")]
+	[int] $major,
+	[parameter(mandatory=$true, parametersetname="GivenNumber", helpmessage="The minor part of the version")]
+	[int] $minor,
+	[parameter(mandatory=$true, parametersetname="GivenNumber", helpmessage="The build part of the version")]
+	[int] $build,
+	[parameter(mandatory=$true, parametersetname="GivenNumber", helpmessage="The revision part of the version")]
+	[int] $revision,
+	[parameter(mandatory=$true, valuefrompipeline=$true, parametersetname="GivenVersionObject", helpmessage="A object with Major, Minor, Build and Revision properties")]
+	$InputObject
+)
+
+if( $InputObject -ne $null ) {
+	$major = $InputObject.Major
+	$minor = $InputObject.Minor
+	$build = $InputObject.Build
+	$revision = $InputObject.Revision
+}
+
+Write-Host "Updating version to $major.$minor.$build.$revision"
+
+$lines = (gc $assemblyInfoFile) -replace "(?<=Assembly(File)?Version(Attribute)?\s*\(\s*`")(\d+\.\d+\.\d+\.\d+)(?=`"\s*\))", "$major.$minor.$build.$revision"
+$lines | sc $assemblyInfoFile
+$newVersion = iex "$($MyInvocation.MyCommand.Path | Split-Path)\get-assemblyversion.ps1 -assemblyInfoFile $assemblyInfoFile"
+
+if( $newVersion.Major -ne $major -or $newVersion.Minor -ne $minor -or $newVersion.Build -ne $build -or $newVersion.Revision -ne $revision ) {
+	throw "Error setting AssemblyVersion. Expected version $major.$minor.$build.$revision but $($newVersion.Major).$($newVersion.Minor).$($newVersion.Build).$($newVersion.Revision) was saved to the file $assemblyInfoFile"
+}
