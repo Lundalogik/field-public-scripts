906f2380963e676a8fd3fb595559afb7a84c0009
Johan Andersson <johan-spam@andersson.net>
2012-10-12 10:34:55 +0200

    Script to get a brief patch status report for Windows Update

---
+++ Windows/Check-ForUpdates.ps1
@@ -0,0 +1,10 @@
+﻿#
+# Use any variant of Invoke-Command (with -Session or -ComputerName) to check multiple target in one command:
+# Invoke-Command -Session (Get-PSSession) -FilePath Check-ForUpdates.ps1
+#
+# If you are using WSUS you might want to look at:
+# http://blogs.technet.com/b/heyscriptingguy/archive/2012/01/16/introduction-to-wsus-and-powershell.aspx
+#
+$UpdateSession = New-Object -ComObject Microsoft.Update.Session
+$updates = $UpdateSession.CreateUpdateSearcher().Search("IsAssigned=1 and IsHidden=0 and IsInstalled=0") | select -ExpandProperty Updates
+$updates | select Title, MsrcSeverity, @{Name="ComputerName";Expression={ $env:COMPUTERNAME }} | group MsrcSeverity,ComputerName | sort Name
