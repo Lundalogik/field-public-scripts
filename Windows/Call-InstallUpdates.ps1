d536b5ad0a8dc9138cc3b6552f601955275d4c23
Johan Andersson <johan-spam@andersson.net>
2012-11-13 02:02:34 +0100

    COM interface for WU is broken and returns E_ACCESSDENIED even in an elevated env. However, running Install-Updates.ps1 as a SYSTEM task works over WinRM

---
+++ Windows/Call-InstallUpdates.ps1
@@ -0,0 +1,12 @@
+﻿$ScriptDir = $MyInvocation.MyCommand.Path | split-path
+Set-Alias registerTask $ScriptDir\Register-TaskForWindowsUpdate.ps1
+Set-Alias getTask $ScriptDir\Get-Task.ps1
+Set-Alias checkForPendingReboot $ScriptDir\Check-ForPendingReboot.ps1
+$task = registerTask
+$task.Run()
+do {
+	sleep -Seconds 10
+	$task = getTask $task.TaskName
+	Write-Host ("Windows Update task is {0}" -f $task.Status)
+} while( $task.Status -eq "Running" )
+checkForPendingReboot
---
+++ Windows/Register-TaskForWindowsUpdate.ps1
@@ -0,0 +1,44 @@
+﻿$ScriptDir = $MyInvocation.MyCommand.Path | split-path
+Set-Alias getTask $ScriptDir\Get-Task.ps1
+
+$ST = new-object -com("Schedule.Service")
+$ST.connect( $env:COMPUTERNAME )
+$RootFolder = $ST.getfolder("\")
+$TaskXml = @"
+<?xml version="1.0" ?>
+<Task xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+    <RegistrationInfo>
+        <Date>$(get-date -format s)</Date>
+        <Author>RemoteX Technologies</Author>
+        <Version>1.0.0</Version>
+        <Description>Starts Windows Update on demand which is needed to be run as SYSTEM which can't be done when invoking it over WinRM.</Description>
+    </RegistrationInfo>
+    <Triggers>
+    </Triggers>
+    <Principals>
+        <Principal>
+            <UserId>SYSTEM</UserId>
+            <LogonType>InteractiveToken</LogonType>
+        </Principal>
+    </Principals>
+    <Settings>
+        <Enabled>true</Enabled>
+        <AllowStartOnDemand>true</AllowStartOnDemand>
+        <AllowHardTerminate>true</AllowHardTerminate>
+    </Settings>
+    <Actions>
+        <Exec>
+	      <Command>%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
+    	  <Arguments>-NonInteractive -Command "&amp; '.\Install-Updates.ps1' -AllowReboot -Force"</Arguments>
+		  <WorkingDirectory>c:\RemoteX\Scripts</WorkingDirectory>
+        </Exec>
+    </Actions>
+</Task>
+"@
+$TASK_CREATE_OR_UPDATE = 6
+$TASK_LOGON_SERVICE_ACCOUNT = 5
+$taskName = "Trigger Installation of Windows Updates"
+$Rootfolder.RegisterTask( $taskName, $TaskXml, $TASK_CREATE_OR_UPDATE, "NT AUTHORITY\SYSTEM", $null, $TASK_LOGON_SERVICE_ACCOUNT ) | Out-Null
+
+# Return task object with Run() method
+getTask $taskName
