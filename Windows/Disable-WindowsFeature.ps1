17c067f98ef8f6fbdb14b5373ea5ec45094dbb9b
Johan Andersson <johan-spam@andersson.net>
2012-11-13 13:42:14 +0100

    Added wrappers for DISM.EXE to enable/disable Windows features. Win7/Win2k8 are known to work with these scripts

---
+++ Windows/Disable-WindowsFeature.ps1
@@ -0,0 +1,33 @@
+﻿param(
+	[parameter(mandatory=$true)]
+	[String[]] $features
+)
+
+$availableFeatures = @{}
+dism /online /get-features /format:table | %{
+	if( $_ -imatch "^([^\|]+).+(Enabled|Disabled)" ) {
+		$availableFeatures.Add( $matches[1].Trim(), $matches[2].Trim() )
+	}				
+}
+if( $LASTEXITCODE -ne 0 ) {
+	throw "DISM returned error $LASTEXITCODE - Are you running in non-elevated mode?"
+}
+
+$toUninstall = @()
+$features | %{
+	if( $availableFeatures.ContainsKey( $_ ) ) {
+		if( $availableFeatures[$_] -eq "Disabled" ) {
+			Write-Host "$_ is already disabled"
+		} else {
+			$toUninstall += $_
+		}
+	} else {
+		Write-Host "Error: Feature $_ does not exist on this machine"
+	}
+}
+if( !$toUninstall ) {
+	Write-Host "All features are already installed"
+} else {
+	Write-Host "Disabling $(($toUninstall | measure).Count) features: $toUninstall"
+	Invoke-Expression "dism /norestart /online /disable-feature /featurename:$([string]::join(' /featurename:', $toUninstall))"
+}
---
+++ Windows/Enable-WindowsFeature.ps1
@@ -0,0 +1,33 @@
+﻿param(
+	[parameter(mandatory=$true)]
+	[String[]] $features
+)
+
+$availableFeatures = @{}
+dism /online /get-features /format:table | %{
+	if( $_ -imatch "^([^\|]+).+(Enabled|Disabled)" ) {
+		$availableFeatures.Add( $matches[1].Trim(), $matches[2].Trim() )
+	}				
+}
+if( $LASTEXITCODE -ne 0 ) {
+	throw "DISM returned error $LASTEXITCODE - Are you running in non-elevated mode?"
+}
+
+$toInstall = @()
+$features | %{
+	if( $availableFeatures.ContainsKey( $_ ) ) {
+		if( $availableFeatures[$_] -eq "Enabled" ) {
+			Write-Host "$_ is already enabled"
+		} else {
+			$toInstall += $_
+		}
+	} else {
+		Write-Host "Error: Feature $_ does not exist on this machine"
+	}
+}
+if( !$toInstall ) {
+	Write-Host "All features are already installed"
+} else {
+	Write-Host "Enabling $(($toInstall | measure).Count) features: $toInstall"
+	Invoke-Expression "dism /norestart /online /enable-feature /featurename:$([string]::join(' /featurename:', $toInstall))"
+}
