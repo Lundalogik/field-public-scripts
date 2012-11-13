3766b4b56a2a4618d850f2bb50a8f7dd8034bc24
Johan Andersson <johan-spam@andersson.net>
2012-11-13 01:32:33 +0100

    Enables automatic updates on a clean install of windows

---
+++ Windows/Enable-AutomaticUpdates.ps1
@@ -0,0 +1,9 @@
+﻿# Turns on automatic updates
+Write-Host "Turning on automatic update setting in registry"
+New-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' -name 'AUOptions' -value '4' -propertyType "DWord" -force | Out-Null
+Write-Host -NoNewline "Enabling service.."
+$AU = New-Object -com "Microsoft.Update.AutoUpdate"
+$AU.EnableService()
+Set-Service wuauserv -StartupType Automatic
+Start-Service wuauserv
+Write-Host "Done"
