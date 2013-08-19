953aa33ecc8c223d128d6cd720f9ce2975701e6a
Johan Andersson <johan-spam@andersson.net>
2013-08-19 22:05:13 +0200

    Gets finished stories from Git commit messages

---
+++ PivotalTracker/Get-FinishedStoriesFromGitCommits.ps1
@@ -0,0 +1,18 @@
+﻿param(
+	[parameter(mandatory=$true)]
+	$sinceCommit
+)
+$commits = git log --pretty=format:"%h(==)%an(==)%ai(==)%s" -i --grep "\[Fi" "${sinceCommit}.."
+$storyinfo = $commits | %{
+	$commit = $_.Split("(==)", [StringSplitOptions]::RemoveEmptyEntries)
+	$subject = $commit[3]
+	$storyids = $subject | select-string "(?<=#)\d+" -AllMatches | select -ExpandProperty Matches | select -ExpandProperty Value
+	New-Object psobject -Property @{ `
+		"Commit" = $commit[0]; `
+		"Author" = $commit[1]; 
+		"Date" = [DateTime]::Parse( $commit[2] );
+		"Subject" = $subject;
+		"Stories" = $storyids | %{ New-Object psobject -Property @{ "id" = $_ } } }
+}
+
+$storyinfo
