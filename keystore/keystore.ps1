50d9e63a62acd6082a0364cb4b192496416d3c03
Johan Andersson <johan-spam@andersson.net>
2013-09-20 23:16:35 +0200

    Avoids MethodException MethodCountCouldNotFindBest when cert is null

--- keystore/keystore.ps1
+++ keystore/keystore.ps1
@@ -39,7 +39,11 @@ filter SHA {
 	$str.ToString()
 }
 
-function PKCSEncrypt( [string] $stringToEncrypt, [System.Security.Cryptography.X509Certificates.X509Certificate2] $cert)
+function PKCSEncrypt( 
+	[parameter(mandatory=$true)] 
+	[string] $stringToEncrypt, 
+	[parameter(mandatory=$true)] 
+	[System.Security.Cryptography.X509Certificates.X509Certificate2] $cert)
 {
     $passbytes = [Text.Encoding]::UTF8.GetBytes($stringToEncrypt)
     $content = New-Object Security.Cryptography.Pkcs.ContentInfo -argumentList (,$passbytes)
@@ -49,7 +53,11 @@ function PKCSEncrypt( [string] $stringToEncrypt, [System.Security.Cryptography.X
     [Convert]::Tobase64String($env.Encode())
 }
 
-function PKCSDecrypt( [string] $EncryptedString, [System.Security.Cryptography.X509Certificates.X509Certificate2] $cert)
+function PKCSDecrypt( 
+	[parameter(mandatory=$true)] 
+	[string] $EncryptedString, 
+	[parameter(mandatory=$true)] 
+	[System.Security.Cryptography.X509Certificates.X509Certificate2] $cert)
 {
     $encodedBytes = [Convert]::Frombase64String($EncryptedString)
     $env = New-Object Security.Cryptography.Pkcs.EnvelopedCms
