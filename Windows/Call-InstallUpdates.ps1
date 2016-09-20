de46cd0c758436541dd652490e02e57227e92fed
Johan Andersson <johan-spam@andersson.net>
2016-09-20 19:04:57 +0200

    Add option to automatically reboot as needed

--- Windows/Call-InstallUpdates.ps1
+++ Windows/Call-InstallUpdates.ps1
@@ -1,4 +1,6 @@
-param( [switch] $OptionalUpdates )
+param(
+  [switch] $OptionalUpdates,
+  [switch] $RebootIfNeeded )
 
 Set-Alias registerTask ( join-path -resolve $PSScriptRoot "Register-TaskForWindowsUpdate.ps1" )
 Set-Alias getTask ( join-path -resolve $PSScriptRoot "Get-Task.ps1" )
@@ -10,4 +12,15 @@ do {
 	$task = getTask $task.TaskName
 	Write-Host ("Windows Update task is {0}" -f $task.Status)
 } while( $task.Status -eq "Running" )
-checkForPendingReboot
+$needsReboot = checkForPendingReboot
+
+Write-Host "Reboot needed: $needsReboot"
+
+if( $needsReboot ) {
+  if( $RebootIfNeeded ) {
+    Write-Host "A reboot is needed and option to automatically reboot is enabled, calling Restart-Computer"
+    Restart-Computer -Force
+  } else {
+    Write-Host "A reboot is needed but the option to automatically reboot was not used. Please perform a reboot manually"
+  }
+}
